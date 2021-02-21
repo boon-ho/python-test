04b4379aa7294b4bca11fab05fcad6a37a31bd06
Boon Ho <ho.boon@gmail.com>
2021-02-21 13:15:04 -0700

    change terminal color to be the same as input paramter

--- hellocli.py
+++ hellocli.py
@@ -10,7 +10,7 @@ def callcolor(color):
     result = with_input(color)
     statement = f"""My color is: {result["old"]}, 
     but I also like {result["new"]}"""
-    click.echo(click.style(statement, bg='red', fg="white"))
+    click.echo(click.style(statement, bg=result["old"], fg="white"))
     sys.exit(0)
     #click.echo(result)
     #click.echo(click.style(f'{result}', bg='green')) 
